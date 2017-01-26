e792750bfdf0afc42aa1b01749090e3a7e2a27c6
Matchoo95 <mattmatth123@googlemail.com>
2017-01-26 17:26:40 +0000

    Initial index.php
    First copy of home page with basic layout

---
+++ Website/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+<title>Edu Home</title>
+
+</head>
+
+<body>
+
+<header>
+      <nav>
+        <ul>
+           <li class="active"><a href="#home">Home</a></li>
+           <li ><a href="#search">Search</a></li>
+           <li ><a href="#login">Login</a></li>
+           <li ><a href="#advertise">Advertise</a></li>
+           <li ><a href="#about">About</a></li>
+        </ul>
+      </nav>
+</header>
+
+<main>
+
+</main>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
