03e27642456d8634f827c7b7cfc52ab28ecfd82e
Matchoo95 <mattmatth123@googlemail.com>
2017-01-27 22:17:59 +0000

    Updated Homepage and added js file
    Add css and html for navigation bar, footer, and buttons.
    Still very basic at the moment, needs work.

--- Website/index.php
+++ Website/index.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="UTF-8"/>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
-<title>Edu Home</title>
-
+  <head>
+    <title>Edu Home</title>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
-
-<header>
-      <nav>
-        <ul>
-           <li class="active"><a href="#home">Home</a></li>
-           <li ><a href="#search">Search</a></li>
-           <li ><a href="#login">Login</a></li>
-           <li ><a href="#advertise">Advertise</a></li>
-           <li ><a href="#about">About</a></li>
-        </ul>
-      </nav>
-</header>
-
-<main>
-
-</main>
-
-<footer>
-
-</footer>
-
-</body>
+  <header>
+    <h1 class="logo">
+      <a href="index.php">Edu Home</a> <!--feel free to change to a image later-->
+    </h1>
+        <nav class="navigation">
+          <ul>
+             <li class="active"><a href="#home">Home</a></li>
+             <li ><a href="#search">Search</a></li>
+             <li ><a href="#advertise">Advertise</a></li>
+             <li ><a href="#about">About</a></li>
+             <li ><a href="#login">Login</a></li> <!--is this going to be a
+               button on the left hand side of the nav bar instead?-->
+          </ul>
+        </nav>
+  </header>
+    <main>
+      <a href="search.php" class="button">I am Looking for Accomodation</a>
+      <a href="advertise.php" class="button">I am Want to Advertise Accomodation</a>
+    </main>
+    <footer>
+      <p>
+        FOOTER
+      </p>
+    </footer>
+  </body>
 </html>
