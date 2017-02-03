6ad923f1ac8aa0be5bfb75c6c62d9270567b1d77
Matchoo95 <mattmatth123@googlemail.com>
2017-02-03 23:10:24 +0000

    SQL changes
    Updated SQL to reflect the SQL used on phpMyAdmin on the database server.

--- Website/Database Code/tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Users
-(
-    firstname varchar(50) NOT NULL,
-    lastname varchar(50) NOT NULL,
-    email varchar(50) NOT NULL UNIQUE,
-    username varchar(50) NOT NULL,
-    password varchar(50) NOT NULL,
-    PRIMARY KEY (username)
-    );
