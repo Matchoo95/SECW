28ada9ef793c879fa3a61d839ae88ee673f1152a
Matchoo95 <mattmatth123@googlemail.com>
2017-02-10 10:18:23 +0000

    added icon/logo

--- Website/index.php
+++ Website/index.php
@@ -8,6 +8,7 @@
 <html lang="en">
   <head>
     <title>Edu Home</title>
+    <link rel="icon" href="images/house-logo.png">
     <meta charset="UTF-8"/>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
