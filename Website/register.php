035a1d190bab25f3f4fcabf109efa74e0994d66e
Matchoo95 <mattmatth123@googlemail.com>
2017-02-14 15:40:49 +0000

    fixed bug

--- Website/register.php
+++ Website/register.php
@@ -9,7 +9,7 @@ if (isset($_POST['username']) && isset($_POST['password'])){
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "INSERT INTO `db667536964`.`Users` (`firstname`, `lastname`, `email`, `phone`, `username`, `password`) VALUES ('$firstname', '$lastname', '$email', '$username', '$password')";
+    $sql = "INSERT INTO `db667536964`.`Users` (`firstname`, `lastname`, `email`, `phone`, `username`, `password`) VALUES ('$firstname', '$lastname', '$email', '$phone', '$username', '$password')";
     $result = mysqli_query($connect, $sql);
     if($result){
       $passmsg = "Your Account Has Been Created.";
