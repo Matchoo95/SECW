4efcc1bc3729715aa2ad813d8b4224ef10bf649d
GuillyVdS <guillaume.vds.97@gmail.com>
2017-04-28 20:23:15 +0100

    Add files via upload

---
+++ Website/edits/search.php
@@ -0,0 +1,101 @@
+<?php
+session_start();
+include './auth.php';
+?>
+<html lang="en">
+  <head>
+    <title>Edu Home</title>
+    <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    <meta charset="UTF-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+  <header>
+    <h1 class="logo">
+  </header>
+    </h1>
+        <nav class="navigation">
+          <a href="index.php"><img src="images/logo.jpg" alt="Edu Home" height="100" width="200"></a>
+          <ul>
+             <li class="active"><a href="index.php">Home</a></li>
+             <li ><a href="search.php">Search</a></li>
+             <li ><a href="advertise.php">Advertise</a></li>
+             <li ><a href="about.php">About</a></li>
+             <li ><a href="register.php">Register</a></li>
+          </ul>
+        </nav>
+
+    <main>
+        <section class="loginBar">
+          <article id="signin">
+            <?php
+              include './login.php';
+              include './hide_login.php';
+            ?>
+            <form id='login' method='post' accept-charset='UTF-8'>
+              <h2>Sign in</h2>
+
+              <label for='username'>Username:</label>
+              <input type='text' name='username' id='username' maxlength="50" placeholder="Username" required autofocus/>
+
+              <br />
+
+              <label for='password'>Password:</label>
+              <input type='password' name='password' id='password' maxlength="50" placeholder="Password" required/>
+
+              <br />
+
+              <button type='submit' name='Submit' value='Submit'>Sign in</button>
+
+            </form>
+          </article>
+        </section>
+        <section id="searchResults" class="searchResults">
+          <form action="search_results.php" method="post">
+            <fieldset>
+              <legend><h3>Search</h3></legend>
+                <p>Please enter criteria for your search.</p>
+                <label for="location">Location</label>
+                <input type="text" name="location" />
+                <label for="type">Type</label>
+                <select name="type">
+                  <option value="Flat" selected>Flat</option>
+                  <option value="Detached">Detached</option>
+                  <option value="Semi-detached">Semi-detached</option>
+                  <option value="Terraced">Terraced</option>
+                  <option value="Bungalow">Bungalow</option>
+                </select>
+                <label for="bedroom">Bedrooms</label>
+                <select name="bedroom">
+                  <option value="1" selected>1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+                <label for="min">Min Price</label>
+                <input type="number" name="min" />
+                <label for="max">Max Price</label>
+                <input type="number" name="max" />
+                    <br />
+                <input type="submit" value="Search" />
+            </fieldset>
+          </form>
+        </section>
+    </main>
+    <footer>
+      <p>
+        Edu Home
+      </p>
+    </footer>
+  </body>
+  <script src="javascript/script.js"></script>
+</html>
---
+++ Website/edits/user_settings.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+include './auth.php';
+include './login.php';
+include './hide_login.php';
+include './login_check.php';
+?>
+<!DOCTYPE html>
+<html lang ="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Change Settings</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <main>
+      <header>
+
+      </header>
+
+
+
+      <nav class="navigation">
+        <a href="index.php"><img src="images/logo.jpg" alt="Edu Home" height="100" width="200"></a>
+        <ul>
+
+           <li class="active"><a href="index.php">Home</a></li>
+           <li ><a href="search.php">Search</a></li>
+           <li ><a href="advertise.php">Advertise</a></li>
+           <li ><a href="about.php">About</a></li>
+           <li ><a href="register.php">Register</a></li>
+        </ul>
+      </nav>
+      <section class="loginBar">
+        <article id="signin">
+        </article>
+      </section>
+      <section>
+          <h3>Change Settings</h3>
+          <form action="change_settings.php" method="post" accept-charset='UTF-8'>
+            New Email: <input type="text" name="email"><br>
+            New Password: <input type="text" name="password"><br>
+            <button type='submit' name='change' value='Change Settings'>Change Settings</button>
+          </form>
+      </section>
+
+        <section>
+          <h4>Terminate account</h4>
+          <form action="delete_account.php" method='post' accept-charset='UTF-8'>
+            <button type='submit' name='delete' value='Delete Account'>Delete Account</button>
+          </form>
+        </section>
+  </main>
+  <footer>
+    <p>
+      Edu Home
+    </p>
+  </footer>
+  </body>
+</html>
